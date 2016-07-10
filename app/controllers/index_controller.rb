983c203a91efd00b94db29ded6a2bed8e757e122
Ifat Ribon <Ifat Ribon>
2016-07-10 15:27:02 -0500

    error handling

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -10,6 +10,37 @@ end
 
 post "/" do
 @word = params[:word]
-redirect "/anagrams/#{@word}"
+begin
+	valid_input(@word)
+	redirect "/anagrams/#{@word}"
+rescue Exception => error
+	@error = error.message
+	erb :index
+end
 end
 
+def three_letters?(input)
+if (input.length <= 3)
+return true
+else 
+return false
+end
+end
+
+def distinct_letters?(input)
+letter_array = input.chars
+unique_letters = letter_array.uniq
+if unique_letters.length < letter_array.length
+false
+else
+true
+end
+end
+
+def valid_input(input)
+if three_letters?(input) && distinct_letters?(input)
+true
+else
+	raise Exception.new("Oops! You should enter a word with three or fewer unique letters.")
+end
+end
