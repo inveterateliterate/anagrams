376547796850a62b8e97320c67a2ab628dca7a93
Ifat Ribon <Ifat Ribon>
2016-07-16 17:47:56 -0500

    adding ability to edit dictionary words

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,9 +1,7 @@
-get "/words" do
-	@words = Word.all
-erb :"/words/index"
-end
+
 
 get "/words/new" do
+@word = Word.new
 erb :"/words/new"
 end
 
@@ -12,7 +10,33 @@ post "/words" do
 	redirect "/words/#{@word.id}"
 end
 
+
+get "/words/:id/edit" do
+	@word = Word.find(params[:id])
+erb :"/words/edit"
+end
+
+put "/words/:id" do
+	@word = Word.find(params[:id])
+	@word.text = params[:word]
+
+if @word.valid?
+	@word.save
+	redirect "/words/#{@word.id}"
+else
+	@word.errors.full_messages.each do |msg|
+		@errors = "#{@errors} #{msg}."
+	end
+	erb :"/words/edit"
+end
+end
+
 get "/words/:id" do 
 	@word = Word.find(params[:id])
 erb :"/words/show"
+end
+
+get "/words" do
+	@words = Word.all.sort
+erb :"/words/index"
 end
