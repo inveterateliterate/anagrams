2871e8a4975cd4013fe5e14ab0d9b2abf2162eb1
Ifat Ribon <Ifat Ribon>
2016-07-16 14:58:03 -0500

    Adding before_create callback to add letters before word creation

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,5 +1,13 @@
 class Word < ActiveRecord::Base
 
+before_create :add_letters
+
+def add_letters
+	characters = self.text.chars
+	alphabetized_characters = characters.sort
+	self.letters = alphabetized_characters.join
+end
+
 def self.find_anagrams(string)
 
 letters = string.split(//)
